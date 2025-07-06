6fb40e65e7124c3766e5d5bb97641bc7d622fc51
Renato T M Lima <renato.matura@gmail.com>
2025-07-06 18:02:29 -0400

    feat(web): Configured theme and setup style guide

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,7 +6,59 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...theme.fontFamily.sans],
+        sans: ["Open Sans", ...theme.fontFamily.sans],
+      },
+      colors: {
+        blue: {
+          base: "#2C46B1",
+          dark: "#2C4091",
+        },
+        gray: {
+          100: "#F9F9FB",
+          200: "#E4E6EC",
+          300: "#CDCFD5",
+          400: "#74798B",
+          500: "#4D505C",
+          600: "#1F2025",
+        },
+        danger: "#B12C4D",
+      },
+      fontSize: {
+        xl: [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+            fontWeight: "700",
+          },
+        ],
+        lg: [
+          "1.125rem",
+          {
+            lineHeight: "1.5rem",
+            fontWeight: "700",
+          },
+        ],
+        md: [
+          "0.875rem",
+          {
+            lineHeight: "1.125rem",
+            fontWeight: "600",
+          },
+        ],
+        sm: [
+          "0.75rem",
+          {
+            lineHeight: "1rem",
+            fontWeight: "400",
+          },
+        ],
+        xs: [
+          "0.625rem",
+          {
+            lineHeight: "0.875rem",
+            fontWeight: "400",
+          },
+        ],
       },
     },
   },
